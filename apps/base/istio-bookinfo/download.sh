ec73f2d84e9ae9741c04fba0d5f920217fc9d73b
Ryan Yin <xiaoyin_c@qq.com>
2024-08-20 17:15:53 +0800

    feat: istio testing - bookinfo

---
+++ apps/base/istio-bookinfo/download.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/istio/istio/release-1.23/samples/bookinfo/platform/kube/bookinfo.yaml
+wget https://raw.githubusercontent.com/istio/istio/release-1.23/samples/bookinfo/platform/kube/bookinfo-versions.yaml
+wget https://raw.githubusercontent.com/istio/istio/release-1.23/samples/bookinfo/gateway-api/bookinfo-gateway.yaml
