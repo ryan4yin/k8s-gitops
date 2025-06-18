f355d9c59205522965a5cbd9712a758648f05bae
Ryan Yin <xiaoyin_c@qq.com>
2025-06-18 21:24:26 +0800

    chore: update istio-bookinfo

--- apps/base/istio-bookinfo/download.sh
+++ apps/base/istio-bookinfo/download.sh
@@ -1,3 +1,3 @@
-wget https://raw.githubusercontent.com/istio/istio/release-1.23/samples/bookinfo/platform/kube/bookinfo.yaml
-wget https://raw.githubusercontent.com/istio/istio/release-1.23/samples/bookinfo/platform/kube/bookinfo-versions.yaml
-wget https://raw.githubusercontent.com/istio/istio/release-1.23/samples/bookinfo/gateway-api/bookinfo-gateway.yaml
+wget https://raw.githubusercontent.com/istio/istio/release-1.26/samples/bookinfo/platform/kube/bookinfo.yaml
+wget https://raw.githubusercontent.com/istio/istio/release-1.26/samples/bookinfo/platform/kube/bookinfo-versions.yaml
+wget https://raw.githubusercontent.com/istio/istio/release-1.26/samples/bookinfo/gateway-api/bookinfo-gateway.yaml
